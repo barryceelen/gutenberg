0b0c96ad8db09733ac1e6bb4422d868ff36c394c
Rastislav Lamos <lamos.rasto@gmail.com>
2017-05-24 12:30:32 +0200

    Add server-side rendering
    I also had to rename the block from core/latest-posts to
    core/latestposts since I think there's a bug in the server-side matcher
    -- it ignores block comments with hyphens: #882

--- index.php
+++ index.php
@@ -9,6 +9,9 @@
  * @package gutenberg
  */
 
+define( 'GUTENBERG__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'GUTENBERG__BLOCKS_LIBRARY_DIR', GUTENBERG__PLUGIN_DIR . 'blocks/library' );
+
 /**
  * Gutenberg's Menu.
  *
@@ -580,3 +583,8 @@ function the_gutenberg_project() {
 	</div>
 	<?php
 }
+
+function gutenberg_load_blocks_server_side_redering() {
+	require_once GUTENBERG__BLOCKS_LIBRARY_DIR . '/latest-posts/index.php';
+}
+add_action( 'init', 'gutenberg_load_blocks_server_side_redering' );
